797f2ec7f33809d769f05ff4a0cce56cedf00b91
Noor Ahmad Bashery <noorahmadbashery@gmail.com>
2024-07-17 14:14:12 +0430

    this is my first commit after changed layout and java code

--- app/src/main/java/com/example/sqlite_curd_operations/MainActivity.java
+++ app/src/main/java/com/example/sqlite_curd_operations/MainActivity.java
@@ -13,15 +13,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_add,btnupdate, showall;
-
-    EditText ID, UserName, Password;
+    Button btn_add, btnupdate, showall, btnSearch;
+    EditText ID, UserName, Password, searchID;
     ListView lstshowdata;
     DatabaseConnection databaseConnection;
     DataModel datamodel;
-    ArrayAdapter CustomerArrayAdapter;
+    ArrayAdapter<DataModel> CustomerArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,81 +35,69 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        btn_add =findViewById(R.id.btnsave);
-        showall =findViewById(R.id.DataShow);
+        btn_add = findViewById(R.id.btnsave);
+        showall = findViewById(R.id.DataShow);
         ID = findViewById(R.id.et_ID);
-        UserName =findViewById(R.id.et_name);
+        UserName = findViewById(R.id.et_name);
         Password = findViewById(R.id.et_name2);
+        searchID = findViewById(R.id.et_search_id);
+        btnSearch = findViewById(R.id.btnSearch);
         lstshowdata = findViewById(R.id.lstview);
         btnupdate = findViewById(R.id.btnUpdate);
         databaseConnection = new DatabaseConnection(MainActivity.this);
 
-
         btn_add.setOnClickListener(v -> {
-
             try {
-                datamodel  = new DataModel(-1, UserName.getText().toString(), Integer.parseInt(Password.getText().toString()));
+                datamodel = new DataModel(-1, UserName.getText().toString(), Integer.parseInt(Password.getText().toString()));
                 Toast.makeText(MainActivity.this, datamodel.toString(), Toast.LENGTH_SHORT).show();
-            }
-            catch (Exception ex){
+            } catch (Exception ex) {
                 Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                datamodel = new DataModel(-1, "error",0 );
+                datamodel = new DataModel(-1, "error", 0);
             }
 
             boolean success = databaseConnection.addOne(datamodel);
-
-            Toast.makeText(MainActivity.this, "Success="+success, Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Success=" + success, Toast.LENGTH_SHORT).show();
         });
+
         btnupdate.setOnClickListener(v -> {
-
             try {
-                datamodel  = new DataModel(Integer.parseInt(ID.getText().toString()), UserName.getText().toString(), Integer.parseInt(Password.getText().toString()));
+                datamodel = new DataModel(Integer.parseInt(ID.getText().toString()), UserName.getText().toString(), Integer.parseInt(Password.getText().toString()));
                 Toast.makeText(MainActivity.this, datamodel.toString(), Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, datamodel.toString(), Toast.LENGTH_SHORT).show();
-
-                boolean   success = databaseConnection.updateOne(datamodel);
-                Toast.makeText(MainActivity.this, "Success="+success, Toast.LENGTH_SHORT).show();
+                boolean success = databaseConnection.updateOne(datamodel);
+                Toast.makeText(MainActivity.this, "Success=" + success, Toast.LENGTH_SHORT).show();
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error" + ex, Toast.LENGTH_SHORT).show();
             }
-            catch (Exception ex){
-                Toast.makeText(MainActivity.this, "error"+ex, Toast.LENGTH_SHORT).show();
-
-            }
-
-
-
-
         });
-
 
         showall.setOnClickListener(v -> {
-            CustomerArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, databaseConnection.getEveryOne());
+            ArrayList<DataModel> allCustomers = (ArrayList<DataModel>) databaseConnection.getEveryOne();
+            CustomerArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, allCustomers);
             lstshowdata.setAdapter(CustomerArrayAdapter);
-            //Toast.makeText(MainActivity.this,everyone.toString(),Toast.LENGTH_LONG).show();
-
         });
 
         lstshowdata.setOnItemClickListener((parent, view, position, id) -> {
-
-            try{
-                DataModel clickedCustomer =  (DataModel)  parent.getItemAtPosition(position);
-
-                databaseConnection.deleteOne (clickedCustomer);
-
-                Toast.makeText(MainActivity.this, " Data deleted" +clickedCustomer,Toast.LENGTH_SHORT).show();
-
+            try {
+                DataModel clickedCustomer = (DataModel) parent.getItemAtPosition(position);
+                databaseConnection.deleteOne(clickedCustomer);
+                Toast.makeText(MainActivity.this, "Data deleted " + clickedCustomer, Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
             }
-            catch (Exception e){
-
-                Toast.makeText(MainActivity.this,e.toString(),Toast.LENGTH_LONG).show();
-            }
-
-
-
         });
 
-
-
-
-
+        btnSearch.setOnClickListener(v -> {
+            try {
+                int searchId = Integer.parseInt(searchID.getText().toString());
+                DataModel result = databaseConnection.getCustomerById(searchId);
+                if (result != null) {
+                    Toast.makeText(MainActivity.this, "Found: " + result.toString(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Customer not found", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error: " + ex, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
